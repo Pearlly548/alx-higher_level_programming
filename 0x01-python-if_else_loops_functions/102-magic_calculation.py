a4afd59c9d45e053bacec74c520d93bc85e30f6f
Pearlly548 <morrisblessing548@gmail.com>
2023-06-14 07:59:43 -0700

    magic

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+def magic_calculation(a, b, c):
+ """Match bytecode provided by Holberton School."""
+    if a > b:
+        return a - b
+    elif a < b:
+        return (a + b) * c
+    else:
+        return a * b - c
+
