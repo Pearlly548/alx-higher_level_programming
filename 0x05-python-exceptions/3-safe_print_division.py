4b50b3e46da7a51552e3cf2615badf3a5e6c822c
Pearlly548 <morrisblessing548@gmail.com>
2023-06-27 06:09:02 -0700

    safe_print_division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+vi 3-safe_print_division.py
+
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
